ac0977794547e6b20105f0ebbf6f3aeadb51579d
DW-Lau <newam2s.lau@gmail.com>
2018-01-24 19:38:20 +0100

    Création du fichier PHP

---
+++ php/mentionsLeg.php
@@ -0,0 +1,12 @@
+<div id="contacts">	
+	<p>
+		<h3>Contacts</h3>
+			<ul>
+				<li>Tel:</li>
+				<li>Mail:</li>
+				<li>Editeur:</li>
+			</ul>
+			<p> Projet réalisé dans le cadre de la formation Openclassrooms: Develloppeur web junior.
+			</p>
+	</p>
+</div>
