cdac9f4b7ae216d6dfc4511b0b6d131867ce2e42
DW-Lau <newam2s.lau@gmail.com>
2018-02-08 12:01:09 +0100

    subuscription in progress

---
+++ inscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Devenir Membre</title>
+</head>
+<body>
+<aside>
+	<p> Grâce aux formulaire vous pourrez devenir membre et profiter de contenu exclusif. </br>
+	En remplissant, et acceptant l'envoie du formulaire vous acceptez l'utilisation des cookies.</p>
+</aside>
+<section>
+		<form method="post" action="">
+			<label name="nom">Nom:</label>
+			<input type="text" name="nom"/>
+
+			<label name="firstName"> Prénom:</label>
+			<input type="text" name="firstName"/>
+
+			
+		</form>
+</section>
+</body>
+</html>
