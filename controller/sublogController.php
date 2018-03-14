7086f719bc59d4abfe47623b2a79af26b3903daf
DW-Lau <newam2s.lau@gmail.com>
2018-03-14 17:00:16 +0100

    The sql elements were deleted from 'espacemembre.php'to be moved in 'sublogController.php'.

--- controller/sublogController.php
+++ controller/sublogController.php
@@ -1,92 +1,81 @@
 <?php
-				try{
-					$bdd=new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root','');
-				}
-				catch (Exception $e){
-					die('Erreur: ' . $e->getmsg());
-				}
+	try{
+		$bdd=new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root','');
+	}
+	catch (Exception $e){
+		die('Erreur: ' . $e->getmsg());
+	}
+
+$lastname = htmlspecialchars($_POST['lastname']);
+$firstname= htmlspecialchars($_POST['firstname']);
+$pseudo = htmlspecialchars($_POST['pseudo']);//PSEUDO
+$mdp=$_POST['mdp'];//MOT DE PASSE
+$mdp1=$_POST['mdp1'];//CONFIRMATION MOT DE PASSE
+$mail = $_POST['mail'];//ADRESSE MAIL
+//
+if ($mdp==$mdp1) {
 
 
-				$_POST['lastname'] = htmlspecialchars($_POST['lastname']);
-				$_POST['firstname'] = htmlspecialchars($_POST['firstname']);
-				$_POST['pseudo'] = htmlspecialchars($_POST['pseudo']);//PSEUDO
-				$_POST['mdp']=$_POST['mdp'];//MOT DE PASSE
-				$_POST['mdp1']=$_POST['mdp1'];//CONFIRMATION MOT DE PASSE
-				//$_POST['mail'] = htmlspecialchars($_POST['mail']);//ADRESSE MAIL
-				// $_POST['signature']=htmlspecialchars($_POST['signature']);
-	//Vérification pseudo
+}else{
+	echo "les 2 mots de passe ne sont pas correspondant.";
+}
+
+/*MAIL*/
+if ( preg_match ("#^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail']) ) {
+
+}else{
+	echo "Votre mail ne correspond pas à la norme...";
+}
+
+//This condition will check if  all inuput are well filled.
+if ( isset($lastname)&& isset($firstname)&&isset($pseudo)&&($mdp==true)&&($mail== true) ) {
+	$pass_hache = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
 
 
-		// if ($_POST['pseudo']&& $^_POST['mdp1'] sont dans la bdd) {
-				//alors on vérifie que les 2 correspondent entres eux.
-				//SINON, on renvoie vers la page login avec un message d'erreur.
+	$req = $bdd->prepare('INSERT INTO membres(lastname,firstname,pseudo,mail,mdp,date_entree) VALUES(:lastname,:firstname,:pseudo,:mail,:mdp,NOW() )');
+	$req = $bdd->execute(array(
+			'lastname'=$lastname,
+			'firstname'=$firstname,
+			'pseudo'=$pseudo,
+			'mail'=$mail,
+			'mdp'=$pass_hache;
+
+			));
+}
+
+	// 	// if ($_POST['pseudo']&& $^_POST['mdp1'] sont dans la bdd) {
+	// 			//alors on vérifie que les 2 correspondent entres eux.
+	// 			//SINON, on renvoie vers la page login avec un message d'erreur.
 	
-		// }
-		// else
-
-		if(isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['pseudo'])){
-			if (preg_match("#^[a-zA-Z0-9]?#", $_POST['pseudo']) ){
-				if (isset($_POST['mail'])) {
-					if (preg_match("#^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])) {
-						if ($_POST['mdp']==$_POST['mdp1']) {
-							if((preg_match("#^[a-zA-Z0-9]?#", $_POST['mdp']))){
+	// 		if (preg_match("#^[a-zA-Z0-9]?#", $_POST['pseudo']) ){
+	// 			if (isset($_POST['mail'])) {
+	// 				if (preg_match("#^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])) {
+	// 					if ($_POST['mdp']==$_POST['mdp1']) {
+	// 						if((preg_match("#^[a-zA-Z0-9]?#", $_POST['mdp']))){
 										
-								$pass_hache = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+	// 							$pass_hache = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
 
-								$req = $bdd->prepare('INSERT INTO membres(lastname,firstname,pseudo,mail,mdp,date_entree) VALUES(:lastname,:firstname,:pseudo,:mail,:mdp,NOW() )');
+	// 							
 
-								$req->execute(array(
-									'lastname'=>$_POST['lastname'],
-									'firstname'=>$_POST['firstname'],
-									'pseudo'=> $_POST['pseudo'],
-									'mail'=>$_POST['mail'],
-									'mdp'=>$pass_hache
-								));
-								// var_dump($_POST['pseudo']);
-								// var_dump($_POST['firstname']);
-								// var_dump($_POST['lastname']);
-								// var_dump($_POST['mail']);
-								$reponse= $bdd->query('SELECT lastname,firstname,pseudo,mail,signature,date_entree FROM membres');
-	 								var_dump($reponse['pseudo']);
-	 								echo htmlspecialchars($reponse['pseudo'])."!";
-
-							}else {
-								echo "erreur 0";
-							}
-						}else{
-							echo "erreur 12";
-						}
-					}else{
-						echo "mail incorrect";
-					}
-				}else{
-			echo "erreur dans le mail";
-		}
-			}else{
-			echo "erreur dans le mdp";
-		}
-			//end php's check regex pseudo
-		}else{
-			echo "erreur dans le nom";
-		}
+	// 							$req->execute(array(
+	// 								'lastname'=>$_POST['lastname'],
+	// 								'firstname'=>$_POST['firstname'],
+	// 								'pseudo'=> $_POST['pseudo'],
+	// 								'mail'=>$_POST['mail'],
+	// 								'mdp'=>$pass_hache
+	// 							));
+	// 							// var_dump($_POST['pseudo']);
+	// 							// var_dump($_POST['firstname']);
+	// 							// var_dump($_POST['lastname']);
+	// 							// var_dump($_POST['mail']);
+	// 							$reponse= $bdd->query('SELECT lastname,firstname,pseudo,mail,signature,date_entree FROM membres');
+	//  								var_dump($reponse['pseudo']);
+	//  								echo htmlspecialchars($reponse['pseudo'])."!";
+	// 	}
 		//End of first and last name condition.
 
 	
 
 	// 						//---------------VERIFICATION DES INFORMATIONS------------//
 
-	// 						
-	// 					}
-	// 						else{
-	// 					echo 'adresse invalide';
-	// 					}
-	// 				}
-	// 			}
-	// 			else{
-	// 				echo "le mot de passe ne correspond pas à la norme</br>";
-	// 			}
-	// 		}else{
-	// 			echo "mot de passe NON similaire! </br>";}//fin mot de passe
-	// 	}//fin pseudo
-	// }
 
