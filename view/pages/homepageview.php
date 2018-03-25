6a6acf6a18908726e3f71c8886a2958586cef112
DW-Lau <newam2s.lau@gmail.com>
2018-03-25 18:21:57 +0200

    Fix and  add some details on css.

--- view/pages/homepageview.php
+++ view/pages/homepageview.php
@@ -1,28 +1,27 @@
-	<div class="borderDeco">
-				</div>
+<div class="borderDeco">
+</div>
 	
-			<section>
-				<div id="sideDeco">
-					<?php
-						try{
-							$bdd=new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root','');
-						}
-						catch (Exception $e){
-							die('Erreur: ' . $e->getmsg());
-						}
+<section>
+	<div id="sideDeco">
+		<?php
+			try{
+				$bdd=new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root','');
+				}
+			catch (Exception $e){
+				die('Erreur: ' . $e->getmsg());
+			}
 
-						$reponse= $bdd->query('SELECT id,titre,textchap,date_format(date_edition,"%d.%m.%y")as date_fr FROM chapitres ORDER BY date_edition  DESC LIMIT 0,2');
+			$reponse= $bdd->query('SELECT id,titre,textchap,date_format(date_edition,"%d.%m.%y")as date_fr FROM chapitres ORDER BY date_edition  DESC LIMIT 0,2');
 						/*Thoses 4 lines created the <article>*/
-						echo '<article id="chaps">
-									<h3 class="Chapters">Les Chapitres</h3>
-										<p> Retrouvez la liste complète des chapitres, au fur et à mesure des postes.
-										</p>';
+			echo '<article id="chaps">
+					<h3 class="Chapters">Les Chapitres</h3>
+						<p> Retrouvez la liste complète des chapitres, au fur et à mesure des postes.</p>';
 
-						while($donnees=$reponse->fetch() ){
-							echo'<div class="lastsChapts">
-							<img src="Images/Alaska_Railroad.jpg" id="imgThumb" alt="Billet simple pour lAlaska">
-							<a href="pages/chapitre.php?id='.$donnees['id'].'">'. htmlspecialchars($donnees['titre']).'</a>
-								<p> posté le:'.$donnees['date_fr'].'</div></p>' ;
+			while($donnees=$reponse->fetch() ){
+				echo'<div class="lastsChapts">
+						<img src="Images/Alaska_Railroad.jpg" id="imgThumb" alt="Billet simple pour lAlaska">
+						<p><a href="pages/chapitre.php?id='.$donnees['id'].'">'. htmlspecialchars($donnees['titre']).'</a>
+						 - posté le:'.$donnees['date_fr'].'</p></div>' ;
 						}
 
 						echo'</article>';//End of <article>
@@ -41,4 +40,4 @@
 						$rep ->closeCursor();
 					?>
 				</div><!--Fin de sideDeco-->
-			</section>
+	</section>
