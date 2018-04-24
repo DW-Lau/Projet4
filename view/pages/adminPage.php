bc09f4aabd27923845b44af41cc604d93ff9621e
DW-Lau <newam2s.lau@gmail.com>
2018-04-24 16:44:23 +0200

    Results of previous functions have been added to the adminPage.php. Last comment leads to it referent chapter.

--- view/pages/adminPage.php
+++ view/pages/adminPage.php
@@ -1,5 +1,28 @@
-<header>
+<div id="headband">
 	<div id="lastChapUpDate">
-		
+			<?php
+				while ($donnees=$lastChapterPost->fetch()) {
+			?>
+			<h2>Dernier Chapitre: </h2>
+				<?php echo htmlspecialchars($donnees['titre']);?>
+			<?php
+				}
+				$lastChapterPost->closeCursor();
+			?>
 	</div>
-</header>
+	<div id="lastCommUpDate">
+		<h2>Dernier Commentaire:</h2>
+		<?php
+			while ($commentUpdate=$lastCommentPost->fetch() ) {
+		?>
+		<p>
+			<strong> Message:</strong> "<?php echo htmlspecialchars($commentUpdate['contenu']);?> ", <strong> écrit par:</strong> <?php echo htmlspecialchars($commentUpdate['membre']);?>.
+			<br/><strong> Au chapitre:</strong> <?php echo htmlspecialchars($commentUpdate['titre']);?>
+			<a href="./home.php?action=selectionchapitre&amp;id=<?php echo $commentUpdate['id_chap']; ?>">Aller vers</a>
+		</p>
+		<?php
+			}
+				$lastCommentPost->closeCursor();
+		?>
+	</div>
+</div>
