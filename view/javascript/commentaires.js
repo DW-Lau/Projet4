ea0310790bbb9466b614b392be727f1b48b97d89
DW-Lau <newam2s.lau@gmail.com>
2018-05-13 14:38:04 +0200

    Removing old files.

--- view/javascript/commentaires.js
+++
@@ -1,8 +0,0 @@
-var home_ShowHide=getElementsByClassName("hide_Show");
-var commentaires= getElementById('comms');
-commentaires.styles.display="none";
-home_ShowHide.addEventListener("click", function (){
-	home_ShowHide.textContent="Masquer les commentaires";
-	commentaires.styles.display="inline-block";
-	commentaires.styles.height="50px";
-});
